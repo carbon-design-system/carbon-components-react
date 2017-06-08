1db7a1e03280c99c7316b33b6427a7a5566512ff
TJ Egan <twegan@us.ibm.com>
2017-06-08 11:36:24 -0500

    feat(inline-select): add inline-select component

--- .storybook/components/SelectStory.js
+++ .storybook/components/SelectStory.js
@@ -5,13 +5,27 @@ import SelectItem from '../../components/SelectItem';
 import SelectItemGroup from '../../components/SelectItemGroup';
 
 const selectProps = {
-  onBlur: () => { action('blur'); }, // eslint-disable-line no-console
-  onClick: () => { action('click'); }, // eslint-disable-line no-console
-  onFocus: () => { action('focus'); }, // eslint-disable-line no-console
-  onMouseDown: () => { action('mouseDown'); }, // eslint-disable-line no-console
-  onMouseEnter: () => { action('mouseEnter'); }, // eslint-disable-line no-console
-  onMouseLeave: () => { action('mouseLeave'); }, // eslint-disable-line no-console
-  onMouseUp: () => { action('mouseUp'); }, // eslint-disable-line no-console
+  onBlur: () => {
+    action('blur');
+  }, // eslint-disable-line no-console
+  onClick: () => {
+    action('click');
+  }, // eslint-disable-line no-console
+  onFocus: () => {
+    action('focus');
+  }, // eslint-disable-line no-console
+  onMouseDown: () => {
+    action('mouseDown');
+  }, // eslint-disable-line no-console
+  onMouseEnter: () => {
+    action('mouseEnter');
+  }, // eslint-disable-line no-console
+  onMouseLeave: () => {
+    action('mouseLeave');
+  }, // eslint-disable-line no-console
+  onMouseUp: () => {
+    action('mouseUp');
+  }, // eslint-disable-line no-console
   className: 'some-class',
 };
 
@@ -24,7 +38,7 @@ storiesOf('Select', module)
       dissapear and the field will reflect the user's choice. Create Select Item components for each
       option in the list. The example below shows an enabled Select component with three items.
     `,
-    () => (
+    () =>
       <Select
         {...selectProps}
         onChange={action('onChange')} // eslint-disable-line no-console
@@ -40,7 +54,30 @@ storiesOf('Select', module)
           <SelectItem value="option-3" text="Option 3" />
         </SelectItemGroup>
       </Select>
-  ))
+  )
+  .addWithInfo(
+    'inline',
+    `
+      Inline select is for use when there will be multiple elements in a row
+    `,
+    () =>
+      <Select
+        {...selectProps}
+        inline
+        onChange={action('onChange')} // eslint-disable-line no-console
+        id="select-1"
+        defaultValue="placeholder-item"
+      >
+        <SelectItem disabled hidden value="placeholder-item" text="Pick an option" />
+        <SelectItemGroup label="Starter">
+          <SelectItem value="option-1" text="Option 1" />
+          <SelectItem value="option-2" text="Option 2" />
+        </SelectItemGroup>
+        <SelectItemGroup label="Advanced">
+          <SelectItem value="option-3" text="Option 3" />
+        </SelectItemGroup>
+      </Select>
+  )
   .addWithInfo(
     'disabled',
     `
@@ -49,14 +86,14 @@ storiesOf('Select', module)
       dissapear and the field will reflect the user's choice. Create SelectItem components for each
       option in the list. The example below shows an disabled Select component.
     `,
-    () => (
-      <Select disabled {...selectProps} id="select-2" >
+    () =>
+      <Select disabled {...selectProps} id="select-2">
         <SelectItem disabled hidden value="placeholder-item" text="Pick an option" />
         <SelectItem value="option-1" text="Option 1" />
         <SelectItem value="option-2" text="Option 2" />
         <SelectItem value="option-3" text="Option 3" />
       </Select>
-  ))
+  )
   .addWithInfo(
     'no label',
     `
@@ -65,7 +102,7 @@ storiesOf('Select', module)
       dissapear and the field will reflect the user's choice. Create SelectItem components for each
       option in the list. The example below shows a Select component without a label.
     `,
-    () => (
+    () =>
       <Select
         {...selectProps}
         onChange={action('onChange')} // eslint-disable-line no-console
@@ -82,4 +119,4 @@ storiesOf('Select', module)
           <SelectItem value="option-3" text="Option 3" />
         </SelectItemGroup>
       </Select>
-  ));
+  );
