bc748e1863bd592275411c156f47077ca0fdcd31
bthan <bthan@us.ibm.com>
2017-06-14 11:04:48 -0500

    test(StructuredListWrapper): initial tests

---
+++ components/__tests__/StructuredList-test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  StructuredListWrapper,
+  StructuredListHead,
+  StructuredListBody,
+  StructuredListRow,
+  StructuredListContent,
+  StructuredListCell,
+} from '../StructuredList';
+import { shallow } from 'enzyme';
+
+describe('StructuredListWrapper', () => {
+  describe('Renders as expected', () => {
+    const wrapper = shallow(
+      <StructuredListWrapper className="extra-class">hi</StructuredListWrapper>
+    );
+
+    it('should have the expected classes', () => {
+      expect(wrapper.hasClass('bx--structured-list')).toEqual(true);
+    });
+
+    it('Should add extra classes that are passed via className', () => {
+      expect(wrapper.hasClass('extra-class')).toEqual(true);
+    });
+
+    it('By default, should add correct modifier class when border prop is true', () => {
+      wrapper.setProps({ border: true });
+      expect(wrapper.hasClass('bx--structured-list--border')).toEqual(true);
+    });
+
+    it('Should remove the modifier class for border when border prop is false', () => {
+      wrapper.setProps({ border: false });
+      expect(wrapper.hasClass('bx--structured-list--border')).toEqual(false);
+    });
+  });
+});
