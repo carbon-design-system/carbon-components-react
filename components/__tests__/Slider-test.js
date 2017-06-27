dbe43f94651b8edd62784c87b0c3ffe4707912b3
Ian Fleming <fyedka2@gmail.com>
2017-06-27 13:55:47 -0500

    test(Slider): WIP slider tests

--- components/__tests__/Slider-test.js
+++ components/__tests__/Slider-test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Slider from '../Slider';
 import TextInput from '../TextInput';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import 'requestanimationframe';
 
 describe('Slider', () => {
@@ -17,11 +17,11 @@ describe('Slider', () => {
     });
 
     it('has the expected classes', () => {
-      expect(wrapper.find('bx--slider-container')).toEqual(true);
+      expect(wrapper.hasClass('bx--slider-container')).toEqual(true);
     });
 
     it('renders extra classes passed in via className', () => {
-      expect(wrapper.find('.bx--slider')).hasClass('extra-class').toEqual(true);
+      // expect(wrapper.find('.bx--slider')).hasClass('extra-class').toEqual(true);
     });
 
     it('can be disabled', () => {
@@ -35,18 +35,39 @@ describe('Slider', () => {
     });
   });
 
-  describe('events', () => {
-    it('click changes value', () => {
-      const wrapper = mount(
-        <Slider id="slider" className="extra-class" value={50} min={0} max={100} step={1}>
-          <TextInput id="input-for-slider" className="bx-slider-text-input"/>
-        </Slider>
-      );
+  describe('calcValue method', () => {
+    const wrapper = mount(
+      <Slider id="slider" className="extra-class" value={50} min={0} max={100} step={1}>
+      </Slider>
+    );
 
-      const slider = wrapper.find('.bx--slider');
+    it('returns correct value from event with a clientX', () => {
+      const evt = {
+        type: 'click',
+        clientX: '1000',
+      }
+      expect(wrapper.instance().calcValue(evt).newValue).toEqual(100)
+    });
+  });
 
-      slider.simulate('click', { clientX: 400 });
-      expect(wrapper.state().value).toEqual(55);
+  describe('keyDown interactions', () => {
+    const wrapper = mount(
+      <Slider id="slider" className="extra-class" value={50} min={0} max={100} step={1}>
+      </Slider>
+    );
+
+    it('steps the value up on right/up keyDown', () => {
+      const thumb = wrapper.find('.bx--slider__thumb');
+      thumb.simulate('keydown', {which: 38});
+      expect(wrapper.state().value).toEqual(51);
+    });
+
+    wrapper.setProps({ value: 50 });
+
+    it('steps the value up on left/down keyDown', () => {
+      const thumb = wrapper.find('.bx--slider__thumb');
+      thumb.simulate('keydown', {which: 40});
+      expect(wrapper.state().value).toEqual(49);
     });
   });
 });
