fe67484e5356454e978dd0394892e32040e3902c
bthan <bthan@us.ibm.com>
2017-07-27 17:21:37 -0500

    chore(NotificationStory): deprecate Notification

--- .storybook/components/NotificationStory.js
+++ .storybook/components/NotificationStory.js
@@ -10,45 +10,47 @@ const notificationProps = {
     subtitle: 'subtitle',
     caption: 'caption',
     iconDescription: 'describes the close button',
-    style: { minWidth: '30rem', marginBottom: '.5rem' },
+    style: { minWidth: '30rem', marginBottom: '.5rem' }
   },
   inline: {
     onCloseButtonClick: action('onCloseButtonClick'),
     className: 'some-class',
     title: 'this is a title',
     subtitle: 'subtitle',
-    iconDescription: 'describes the close button',
-  },
+    iconDescription: 'describes the close button'
+  }
 };
 
-storiesOf('Notifications', module)
-  .addWithInfo(
-    'toast',
-    `
+storiesOf('Notifications', module).addWithInfo(
+  'Deprecated: <Notfication />',
+  `
       Toast notifications are typically passive, meaning they won't affect the user's workflow if not addressed.
       Toast Notifications use 'kind' props to specify the kind of notification that should render (error, info, success, warning).
     `,
-    () => (
-      <div>
-        <Notification {...notificationProps.toast} kind="error" />
-        <Notification {...notificationProps.toast} kind="info" />
-        <Notification {...notificationProps.toast} kind="success" />
-        <Notification {...notificationProps.toast} kind="warning" />
-      </div>
-    ),
-  )
-  .addWithInfo(
-    'inline',
-    `
-    Inline notifications appear in-page, and are important to the context of what the user's workflow.
-    Inline Notifications use 'kind' props to specify the kind of notification that should render (error, info, success, warning).
-    `,
-    () => (
-      <div>
-        <Notification {...notificationProps.inline} kind="error" />
-        <Notification {...notificationProps.inline} kind="info" />
-        <Notification {...notificationProps.inline} kind="success" />
-        <Notification {...notificationProps.inline} kind="warning" />
-      </div>
-    ),
-  );
+  () =>
+    <div>
+      <Notification {...notificationProps.toast} kind="error" />
+      <Notification {...notificationProps.toast} kind="info" />
+      <Notification {...notificationProps.toast} kind="success" />
+      <Notification {...notificationProps.toast} kind="warning" />
+      <Notification {...notificationProps.inline} kind="error" />
+      <Notification {...notificationProps.inline} kind="info" />
+      <Notification {...notificationProps.inline} kind="success" />
+      <Notification {...notificationProps.inline} kind="warning" />
+    </div>
+);
+// .addWithInfo(
+//   'inline',
+//   `
+//   Inline notifications appear in-page, and are important to the context of what the user's workflow.
+//   Inline Notifications use 'kind' props to specify the kind of notification that should render (error, info, success, warning).
+//   `,
+//   () => (
+//     <div>
+//       <Notification {...notificationProps.inline} kind="error" />
+//       <Notification {...notificationProps.inline} kind="info" />
+//       <Notification {...notificationProps.inline} kind="success" />
+//       <Notification {...notificationProps.inline} kind="warning" />
+//     </div>
+//   ),
+// );
