40a2c1222923ea9076e9488fa29bb226de447ff8
Greg Ferreri <gsferrer@us.ibm.com>
2017-08-14 17:26:45 -0400

    fix: update selected state when prop changes

--- components/__tests__/Tabs-test.js
+++ components/__tests__/Tabs-test.js
@@ -156,7 +156,7 @@ describe('Tabs', () => {
 
     describe('keydown', () => {
       const wrapper = mount(
-        <Tabs>
+        <Tabs selected={0}>
           <Tab label="firstTab" className="firstTab">content</Tab>
           <Tab label="lastTab" className="lastTab">content</Tab>
         </Tabs>,
@@ -193,6 +193,13 @@ describe('Tabs', () => {
           lastTab.simulate('keydown', { which: enterKey });
           expect(wrapper.state().selected).toEqual(1);
         });
+
+        it('updates selected state when selected prop changes', () => {
+          wrapper.setProps({selected: 1});
+          expect(wrapper.state().selected).toEqual(1);
+          wrapper.setProps({selected: 0});
+          expect(wrapper.state().selected).toEqual(0);
+        })
       });
     });
   });
