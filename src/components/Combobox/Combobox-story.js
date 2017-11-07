08c8e3f4eecf0c547bc71ca560f5bdc7e28c3858
Josh Black <joshblack@us.ibm.com>
2017-11-07 12:28:43 -0600

    feat: add Combobox

---
+++ src/components/Combobox/Combobox-story.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Combobox from './Combobox';
+
+const items = [
+  {
+    label: 'Apple',
+    value: 'apple',
+  },
+  {
+    label: 'Banana',
+    value: 'banana',
+  },
+  {
+    label: 'Oranges',
+    value: 'oranges',
+  },
+  {
+    label: 'Pear',
+    value: 'pear',
+  },
+  {
+    label: 'Peanut',
+    value: 'peanut',
+  },
+  {
+    label: 'Watermelon',
+    value: 'watermelon',
+  },
+];
+
+const onChange = evt => {
+  console.log(evt);
+};
+
+storiesOf('Combobox', module)
+  .addWithInfo('Default', `Combobox`, () => (
+    <Combobox
+      id="test"
+      placeholder="Filter..."
+      onChange={onChange}
+      items={items}
+    />
+  ))
+  .addWithInfo('Pre-selected', `Combobox`, () => (
+    <Combobox
+      id="test"
+      placeholder="Filter..."
+      selectedItem={{
+        label: 'Banana',
+        value: 'banana',
+      }}
+      onChange={onChange}
+      items={items}
+    />
+  ))
+  .addWithInfo('Disabled', 'Combobox', () => (
+    <Combobox
+      id="test"
+      placeholder="Filter..."
+      selectedItem={{
+        label: 'Banana',
+        value: 'banana',
+      }}
+      onChange={onChange}
+      items={items}
+      disabled={true}
+    />
+  ))
