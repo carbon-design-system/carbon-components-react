911f8b911432f67747b422cd439ead26759d55ab
Josh Black <joshblack@us.ibm.com>
2017-11-14 16:04:35 -0600

    chore: clean-up multi-select test

--- src/components/MultiSelect/MultiSelect-test.js
+++ src/components/MultiSelect/MultiSelect-test.js
@@ -1,47 +1,9 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import MultiSelect from '../MultiSelect';
-
-// Design spec      -> Visual contract
-// Engineering spec -> API
-
-// Managed MultiSelect that keeps track of selected items internally, can listen
-// to the `onChange` handler to get details of what is happening and subscribe
-// to updates
-/**
- * <MultiSelect
- *   items={[]}
- *   initialSelectedItems={[]}
- *   itemToString={}
- *   onChange={}
- * />
- */
-
-// `GenericItem` corresponds to an item in a collection that is passed to
-// MultiSelect that is in a predictable shape and works with the default
-// `itemTostring` out of the box.
-const generateGenericItem = index => ({
-  id: `id-${index}`,
-  label: `Item ${index}`,
-  value: index,
-});
-
-// `CustomItem` corresponds to a potentially different item structure that
-// might be passed into MultiSelect that we would need to supply a custom
-// `itemToString` method for
-const generateCustomItem = index => ({
-  field: `Item ${index}`,
-  value: `Custom value ${index}`,
-});
-
-const customItemToString = ({ field }) => field;
-
-const generateItems = (amount, generator) =>
-  Array(amount).fill(null).map((_, i) => generator(i));
+import { generateItems, generateGenericItem, generateCustomItem, customItemToString } from '../../tools/testing/items';
 
 describe('MultiSelect', () => {
-  it('should render');
-
   it('should initialize with no selected items if no `initialSelectedItems` are given', () => {
     const items = generateItems(5, generateGenericItem);
     const wrapper = mount(
