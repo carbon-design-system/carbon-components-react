7e659c8accd53da9e92da99c326cc302714d7539
Josh Black <joshblack@us.ibm.com>
2017-12-12 16:26:43 -0600

    feat: add Selection component to encapsulate selection state

---
+++ src/internal/Selection.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Selection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedItems: props.initialSelectedselectedItems || [],
+    };
+  }
+
+  internalSetState = (stateToSet, callback) =>
+    this.setState(stateToSet, () => {
+      if (callback) {
+        callback();
+      }
+      if (this.props.onChange) {
+        this.props.onChange(this.state);
+      }
+    });
+
+  handleClearSelection = () => {
+    this.internalSetState({
+      selectedItems: [],
+    });
+  };
+
+  handleSelectItem = item => {
+    this.internalSetState(state => ({
+      selectedItems: state.selectedItems.concat(item),
+    }));
+  };
+
+  handleRemoveItem = index => {
+    this.internalSetState(state => ({
+      selectedItems: removeAtIndex(state.selectedItems, index),
+    }));
+  };
+
+  handleOnItemChange = item => {
+    const { selectedItems } = this.state;
+    const selectedIndex = selectedItems.indexOf(item);
+
+    if (selectedIndex === -1) {
+      this.handleSelectItem(item);
+      return;
+    }
+    this.handleRemoveItem(selectedIndex);
+  };
+
+  render() {
+    const { children, render } = this.props;
+    const { selectedItems } = this.state;
+    const renderProps = {
+      selectedItems,
+      onItemChange: this.handleOnItemChange,
+      clearSelection: this.handleClearSelection,
+    };
+
+    if (render !== undefined) {
+      return render(renderProps);
+    }
+
+    if (children !== undefined) {
+      return children(renderProps);
+    }
+
+    return null;
+  }
+}
+
+// Generic utility for safely removing an element at a given index from an
+// array.
+const removeAtIndex = (array, index) => {
+  const result = array.slice();
+  result.splice(index, 1);
+  return result;
+};
