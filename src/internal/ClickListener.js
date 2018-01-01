6da0fa57027e196c2aa32bf7c8d93356c4750a0c
Josh Black <joshblack@us.ibm.com>
2018-01-01 12:13:46 -0600

    feat(ClickListener): update ClickListener to not create wrapper node

--- src/internal/ClickListener.js
+++ src/internal/ClickListener.js
@@ -1,13 +1,24 @@
 import PropTypes from 'prop-types';
-/* global document */
-
 import React from 'react';
 
-class ClickListener extends React.Component {
+/**
+ * Generic component used for reacting to a click event happening outside of a
+ * given `children` element.
+ */
+export default class ClickListener extends React.Component {
   static propTypes = {
-    children: PropTypes.node.isRequired,
+    children: PropTypes.element.isRequired,
     onClickOutside: PropTypes.func.isRequired,
   };
+
+  constructor(props) {
+    super(props);
+    // We manually bind handlers in this Component, versus using class
+    // properties, so that we can properly test the `handleRef` handler with
+    // enzyme.
+    this.handleRef = this.handleRef.bind(this);
+    this.handleDocumentClick = this.handleDocumentClick.bind(this);
+  }
 
   componentDidMount() {
     document.addEventListener('click', this.handleDocumentClick);
@@ -17,24 +28,35 @@ class ClickListener extends React.Component {
     document.removeEventListener('click', this.handleDocumentClick);
   }
 
-  handleDocumentClick = evt => {
+  handleDocumentClick(evt) {
     if (this.element) {
-      if (!this.element.contains(evt.target)) {
+      if (this.element.contains && !this.element.contains(evt.target)) {
         this.props.onClickOutside(evt);
       }
     }
-  };
+  }
+
+  handleRef(el) {
+    const { children } = this.props;
+    this.element = el;
+
+    /**
+     * One important note, `children.ref` corresponds to a `ref` prop passed in
+     * directly to the child, not necessarily a `ref` defined in the component.
+     * This means that here we target the following `ref` location:
+     *
+     * <ClickListener onClickOutside={() => {}}>
+     *   <Child ref={targetedRefHere} />
+     * </ClickListener>
+     */
+    if (children.ref && typeof children.ref === 'function') {
+      children.ref(el);
+    }
+  }
 
   render() {
-    return (
-      <div
-        ref={el => {
-          this.element = el;
-        }}>
-        {this.props.children}
-      </div>
-    );
+    return React.cloneElement(this.props.children, {
+      ref: this.handleRef,
+    });
   }
 }
-
-export default ClickListener;
