a0f14137b804f53ef5027411049a68e6e9ba97cb
mjohannessen <mari.l.johannessen@gmail.com>
2018-01-09 11:49:43 -0600

    fix(interior-left-nav): added active href check

--- src/components/InteriorLeftNavItem/InteriorLeftNavItem.js
+++ src/components/InteriorLeftNavItem/InteriorLeftNavItem.js
@@ -21,8 +21,10 @@ const InteriorLeftNavItem = ({
 }) => {
   const childHref =
     children.props.href === undefined ? children.props.to : children.props.href;
+  const activePath =
+    window.location && window.location.hash ? window.location.hash : activeHref;
   const classNames = classnames('left-nav-list__item', className, {
-    'left-nav-list__item--active': activeHref === childHref,
+    'left-nav-list__item--active': activePath === childHref,
   });
 
   return (
