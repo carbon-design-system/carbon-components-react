6c1f345ff555456301e423cd726c88abac6ee732
Josh Black <joshblack@us.ibm.com>
2018-02-09 14:36:58 -0600

    docs(InlineCheckbox): add prop-types annotations

--- src/components/InlineCheckbox/InlineCheckbox.js
+++ src/components/InlineCheckbox/InlineCheckbox.js
@@ -17,10 +17,29 @@ const InlineCheckbox = ({ id, checked, name, onClick, onKeyDown }) => (
 );
 
 InlineCheckbox.propTypes = {
+  /**
+   * Provide an `id` for the underlying input control
+   */
   id: PropTypes.string.isRequired,
+
+  /**
+   * Provide a `name` for the underlying input control
+   */
   name: PropTypes.string.isRequired,
+
+  /**
+   * Provide a handler that is invoked when a user clicks on the control
+   */
   onClick: PropTypes.func,
+
+  /**
+   * Provide a handler that is invoked on the key down event for the control
+   */
   onKeyDown: PropTypes.func,
+
+  /**
+   * Specify whether the underlying control is checked, or not
+   */
   checked: PropTypes.bool.isRequired,
 };
 
