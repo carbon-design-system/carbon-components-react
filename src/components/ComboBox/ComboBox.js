ab3073028390d6cd4fc895f7e4925926fd302ba1
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2018-03-30 15:43:38 -0400

    feat(ComboBox): allow 'initialSelectedItem' prop to be a string

--- src/components/ComboBox/ComboBox.js
+++ src/components/ComboBox/ComboBox.js
@@ -37,10 +37,13 @@ export default class ComboBox extends React.Component {
     id: PropTypes.string,
 
     /**
-     * Allow users to pass in arbitrary items from their collection that are
-     * pre-selected
+     * Allow users to pass in an arbitrary item or a string (in case their items are an array of strings)
+     * from their collection that are pre-selected
      */
-    initialSelectedItem: PropTypes.object,
+    initialSelectedItem: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.string,
+    ]),
 
     /**
      * We try to stay as generic as possible here to allow individuals to pass
