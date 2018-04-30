62f277f58ef0229692accf9143357124b10cb518
Rezak OTMANI <rezak.otmani@yahoo.fr>
2018-04-30 14:13:53 +0200

    fix(DatePicker): show DatePicker when cliking in the icon in single with calendar, resolves #800

--- src/components/DatePicker/DatePicker.js
+++ src/components/DatePicker/DatePicker.js
@@ -260,7 +260,7 @@ export default class DatePicker extends Component {
     });
 
     const datePickerIcon =
-      datePickerType === 'range' ? (
+      datePickerType === 'range' || datePickerType === 'single' ? (
         <svg
           onClick={this.openCalendar}
           className="bx--date-picker__icon"
