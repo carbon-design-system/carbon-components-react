cdf821161459b2d1bf6dbe3f4443336ac1559929
Alex Kim <Alex.Kim@ibm.com@Alexs-MacBook-Pro-2.local>
2018-05-09 18:34:03 -0500

    feat(Tag): add a new type of Tag that is selectable and removable

--- src/components/Tag/Tag.js
+++ src/components/Tag/Tag.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
+import Icon from '../Icon';
 
 const TYPES = {
   beta: 'Beta',
@@ -11,24 +12,118 @@ const TYPES = {
   ibm: 'IBM',
   local: 'Local',
   private: 'Private',
+  functional: 'Functional',
   'third-party': 'Third-Party',
 };
 
-const Tag = ({ children, className, type, ...other }) => {
-  const tagClass = `bx--tag--${type}`;
-  const tagClasses = classNames('bx--tag', tagClass, className);
-  return (
-    <span className={tagClasses} {...other}>
-      {children || TYPES[type]}
-    </span>
-  );
-};
+export default class Tag extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false,
+      removed: false,
+    };
+  }
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    type: PropTypes.oneOf(Object.keys(TYPES)).isRequired,
+    isRemovable: PropTypes.bool,
+    onRemove: PropTypes.func,
+    onSelect: PropTypes.func,
+  };
 
-Tag.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  type: PropTypes.oneOf(Object.keys(TYPES)).isRequired,
-};
+  static defaultProps = {
+    onRemove: () => {},
+    onSelect: () => {},
+    isRemovable: false,
+  };
+
+  toggleSelect = event => {
+    const { onSelect, children } = this.props;
+    const newState = !this.state.selected;
+
+    this.setState({
+      selected: newState,
+    });
+
+    event.stopPropagation();
+    if (onSelect) {
+      onSelect({ children: children, isSelected: newState });
+    }
+  };
+
+  handleRemove = event => {
+    const { onRemove, children } = this.props;
+    event.stopPropagation();
+    this.setState({
+      removed: true,
+    });
+
+    if (onRemove) {
+      onRemove(children);
+    }
+  };
+
+  render() {
+    const {
+      children,
+      className,
+      type,
+      isRemovable,
+      onRemove, // eslint-disable-line no-unused-vars
+      onSelect, // eslint-disable-line no-unused-vars
+      ...other
+    } = this.props;
+    let tagClass = `bx--tag--${type}`;
+    let tagClasses = classNames(
+      'bx--tag',
+      tagClass,
+      {
+        'bx--tag--functional__selected': this.state.selected,
+        'bx--tag__removed': this.state.removed,
+      },
+      className
+    );
+
+    let tagProps = {
+      className: tagClasses,
+      ...other,
+    };
+    if (TYPES[type] === 'Functional') {
+      tagProps = {
+        ...tagProps,
+        className: tagClasses,
+        tabIndex: 0,
+        onClick: this.toggleSelect,
+        onKeyDown: evt => {
+          if (evt.which === 13 || evt.which === 32) this.toggleSelect(evt);
+        },
+      };
+    }
+
+    const closeIcon = (
+      <Icon
+        className="bx--tag-close"
+        name="close"
+        fill="grey"
+        description="This is a description of the icon and what it does…"
+        role="button"
+        tabIndex="0"
+        onClick={this.handleRemove}
+        onKeyDown={evt => {
+          if (evt.which === 13 || evt.which === 32) this.handleRemove(evt);
+        }}
+      />
+    );
+
+    return (
+      <span {...tagProps} style={{ cursor: 'default' }}>
+        {children || TYPES[type]}
+        {isRemovable && closeIcon}
+      </span>
+    );
+  }
+}
 
 export const types = Object.keys(TYPES);
-export default Tag;
