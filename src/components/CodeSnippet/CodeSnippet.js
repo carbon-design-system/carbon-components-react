cc88cae6d5db4be31c3747a6ff1ecac2c910d165
Alison Joseph <alisonejoseph@gmail.com>
2018-05-14 15:14:48 -0500

    feat(code-snippet): Add updated code snippet options

--- src/components/CodeSnippet/CodeSnippet.js
+++ src/components/CodeSnippet/CodeSnippet.js
@@ -1,43 +1,137 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import classNames from 'classnames';
+import Copy from '../Copy';
 import CopyButton from '../CopyButton';
+import Icon from '../Icon';
 
-const CodeSnippet = ({
-  className,
-  type,
-  children,
-  feedback,
-  onClick,
-  wrappedContentRef,
-  ...other
-}) => {
-  const snippetType =
-    type === 'terminal' ? 'bx--snippet--terminal' : 'bx--snippet--code';
-  const wrapperClasses = classNames('bx--snippet', className, snippetType);
-  return (
-    <div className={wrapperClasses} {...other}>
+export default class CodeSnippet extends Component {
+  static propTypes = {
+    /**
+     * The type of code snippet
+     * can be inline, single or multi
+     */
+    type: PropTypes.string,
+    className: PropTypes.string,
+    children: PropTypes.string,
+    feedback: PropTypes.string,
+    copyLabel: PropTypes.string,
+    onClick: PropTypes.func,
+    wrappedContentRef: PropTypes.func,
+    showMoreText: PropTypes.string,
+    showLessText: PropTypes.string,
+  };
+
+  static defaultProps = {
+    type: 'single',
+    showMoreText: 'Show more',
+    showLessText: 'Show less',
+  };
+
+  state = {
+    showBtn: false,
+    expandedCode: false,
+  };
+
+  componentDidMount = () => {
+    if (ReactDOM.findDOMNode(this).offsetHeight > 190) {
+      this.setState({ showBtn: true });
+    }
+  };
+
+  expandCode = () => {
+    this.setState({ expandedCode: !this.state.expandedCode });
+  };
+
+  render() {
+    const {
+      className,
+      type,
+      children,
+      feedback,
+      onClick,
+      copyLabel,
+      wrappedContentRef,
+      /*
+      showMoreText,
+      showLessText,
+      */
+      ...other
+    } = this.props;
+
+    const codeSnippetClasses = classNames(className, {
+      'bx--snippet': true,
+      'bx--snippet--single': (type === 'single') | (type === 'terminal'),
+      'bx--snippet--multi': (type === 'multi') | (type === 'code'),
+      'bx--snippet--inline': type === 'inline',
+      'bx--snippet--expand': this.state.expandedCode,
+    });
+
+    const expandCodeBtnText = this.state.expandedCode ? 'less' : 'more';
+
+    const moreLessBtn = (
+      <button
+        className="bx--btn bx--btn--ghost bx--btn--sm bx--snippet-btn--expand"
+        type="button"
+        onClick={this.expandCode}>
+        <span className="bx--snippet-btn--text">{expandCodeBtnText}</span>
+        <Icon
+          alt={expandCodeBtnText}
+          name="chevron--down"
+          description={expandCodeBtnText}
+          className="bx--icon-chevron--down"
+        />
+      </button>
+    );
+
+    const code = (
       <div role="textbox" tabIndex={0} className="bx--snippet-container">
         <code>
           <pre ref={wrappedContentRef}>{children}</pre>
         </code>
       </div>
-      <CopyButton onClick={onClick} feedback={feedback} />
-    </div>
-  );
-};
+    );
 
-CodeSnippet.propTypes = {
-  type: PropTypes.string,
-  className: PropTypes.string,
-  children: PropTypes.string,
-  feedback: PropTypes.string,
-  onClick: PropTypes.func,
-  wrappedContentRef: PropTypes.func,
-};
+    const copy = <CopyButton onClick={onClick} feedback={feedback} />;
 
-CodeSnippet.defaultProps = {
-  type: 'terminal',
-};
+    if ((type === 'inline') | (type === 'terminal')) {
+      return (
+        <Copy
+          className={codeSnippetClasses}
+          aria-label={copyLabel}
+          feedback={feedback}>
+          <code>{children}</code>
+        </Copy>
+      );
+    }
 
-export default CodeSnippet;
+    if (type === 'single') {
+      return (
+        <div className={codeSnippetClasses} {...other}>
+          {code}
+          {copy}
+        </div>
+      );
+    }
+
+    if ((type === 'multi') | (type === 'code') && !this.state.showBtn) {
+      return (
+        <div className={codeSnippetClasses} {...other}>
+          {code}
+          {copy}
+        </div>
+      );
+    }
+
+    if ((type === 'multi') | (type === 'code') && this.state.showBtn) {
+      return (
+        <div className={codeSnippetClasses} {...other}>
+          {code}
+          {copy}
+          {moreLessBtn}
+        </div>
+      );
+    }
+  }
+}
