20ec0249e38898615b9b56b43e0cd1b368079091
Akira Sudoh <asudoh@gmail.com>
2018-05-22 00:41:32 +0900

    fix(OverflowMenu): fix <ClickOutside> not covering floating menu (#906)
    Fixes #628.

--- src/components/OverflowMenu/OverflowMenu.js
+++ src/components/OverflowMenu/OverflowMenu.js
@@ -269,8 +269,10 @@ export default class OverflowMenu extends Component {
     }
   };
 
-  handleClickOutside = () => {
-    this.closeMenu();
+  handleClickOutside = evt => {
+    if (!this._menuBody || !this._menuBody.contains(evt.target)) {
+      this.closeMenu();
+    }
   };
 
   closeMenu = () => {
@@ -287,8 +289,11 @@ export default class OverflowMenu extends Component {
    * @private
    */
   _bindMenuBody = menuBody => {
-    if (!menuBody && this._hFocusIn) {
-      this._hFocusIn = this._hFocusIn.release();
+    if (!menuBody) {
+      this._menuBody = menuBody;
+      if (this._hFocusIn) {
+        this._hFocusIn = this._hFocusIn.release();
+      }
     }
   };
 
@@ -299,6 +304,7 @@ export default class OverflowMenu extends Component {
    */
   _handlePlace = menuBody => {
     if (menuBody) {
+      this._menuBody = menuBody;
       (
         menuBody.querySelector('[data-floating-menu-primary-focus]') || menuBody
       ).focus();
