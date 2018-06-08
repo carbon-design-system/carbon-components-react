f03989792a6605936a4e7aae7f894716d3c3df3f
TJ Egan <twegan@us.ibm.com>
2018-06-08 10:18:33 -0500

    fix(tooltip): add tooltip simple export

--- src/index.js
+++ src/index.js
@@ -109,6 +109,7 @@ export ToolbarSearch from './components/ToolbarSearch';
 export Tooltip from './components/Tooltip';
 export TooltipDefinition from './components/TooltipDefinition';
 export TooltipIcon from './components/TooltipIcon';
+export TooltipSimple from './components/TooltipSimple';
 export UnorderedList from './components/UnorderedList';
 export SkeletonText from './components/SkeletonText';
 export DataTableSkeleton from './components/DataTableSkeleton';
