8f1f88e6d90512c03d2be3652398151dc89fc439
Matt Wallace <matthew.wallace@cognizant.com>
2018-07-19 15:05:21 -0600

    chore: Reverted by removing new polyfill

--- .storybook/polyfills.js
+++ .storybook/polyfills.js
@@ -3,4 +3,3 @@ import 'core-js/modules/es6.array.fill';
 import 'core-js/modules/es6.string.includes';
 import 'core-js/modules/es6.string.trim';
 import 'core-js/modules/es7.object.values';
-import 'core-js/modules/es6.array.find-index';
