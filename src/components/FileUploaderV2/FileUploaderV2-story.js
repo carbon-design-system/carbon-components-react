e0565556446782f1dde1b5342595f4a5ab08edb5
Andrew Wang <afw5059@gmail.com>
2018-08-08 14:25:59 -0500

    docs(FileUploaderV2): remove extraneous storybook example

--- src/components/FileUploaderV2/FileUploaderV2-story.js
+++ src/components/FileUploaderV2/FileUploaderV2-story.js
@@ -95,35 +95,6 @@ storiesOf('FileUploaderV2', module)
     )
   )
   .addWithInfo(
-    'FileUploader',
-    `
-      The FileUploader components allow the user to upload any necessary files. This uses the FileUploaderButton and Filename components. Filename components will appear below the FileUploaderButton when files are added. Use the status prop to control what icon appears in Filename ('edit', 'complete', or 'uploading'). The FileUploader component contains a method to clear all files uploaded, clearFiles. This can be used with a ref in the parent component. The clear file button in this example is an example of how to use the clearFiles method.
-    `,
-    () => (
-      <div className="bx--file__container">
-        <FileUploaderV2
-          labelTitle="Upload"
-          labelDescription="only .jpg and .png files at 500mb or less"
-          buttonLabel="Add files"
-          status="edit"
-          accept={['.jpg', '.png']}
-          name="file"
-          multiple
-          ref={fileUploader => (this.fileUploader = fileUploader)}
-        />
-        <Button
-          kind="secondary"
-          small
-          style={{ marginTop: '1rem' }}
-          onClick={() => {
-            this.fileUploader.clearFiles();
-          }}>
-          Clear File
-        </Button>
-      </div>
-    )
-  )
-  .addWithInfo(
     'FileUploader example application',
     `
       example application
