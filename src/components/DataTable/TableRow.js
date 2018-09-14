72f13bcabf806036d1dee5879473d93af500bd85
Andrew Wang <afw5059@gmail.com>
2018-09-14 11:50:51 -0500

    refactor(TableRow): attach `className` to `cleanProps`

--- src/components/DataTable/TableRow.js
+++ src/components/DataTable/TableRow.js
@@ -5,12 +5,14 @@ import classNames from 'classnames';
 const TableRow = props => {
   // Remove unnecessary props if provided to this component, these are
   // only useful in `TableExpandRow`
-  const cleanProps = omit(props, ['ariaLabel', 'onExpand', 'isExpanded']);
-  const classes = classNames(props.className, {
+  const className = classNames(props.className, {
     'bx--data-table-v2--selected': props.selected,
   });
-
-  return <tr className={classes} {...cleanProps} />;
+  const cleanProps = {
+    ...omit(props, ['ariaLabel', 'onExpand', 'isExpanded']),
+    className,
+  };
+  return <tr {...cleanProps} />;
 };
 
 export default TableRow;
