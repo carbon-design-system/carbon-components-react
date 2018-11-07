4f9d8d39c5dd660cdb02aca178ad86a70e0eac54
Andrew Wang <afw5059@gmail.com>
2018-11-07 09:08:47 -0600

    fix(PasswordInput): export named password input components

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,2 +1,4 @@
 export * from './TextInput.Skeleton';
+export ControlledPasswordInput from './ControlledPasswordInput';
+export PasswordInput from './PasswordInput';
 export default from './TextInput';
