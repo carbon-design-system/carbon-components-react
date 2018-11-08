7ad10fb0338326e15f1e987403264a9f0cd10e61
Andrew Wang <afw5059@gmail.com>
2018-11-08 13:20:02 -0600

    refactor(Notification): extract a11y warning icon to separate component

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -9,6 +9,8 @@ import {
   iconWarningSolid,
 } from 'carbon-icons';
 import Icon from '../Icon';
+// temporary workaround for a11y warning icon. TODO: for @carbon/icons-react
+import a11yIconWarningSolid from './a11yIconWarningSolid';
 
 export class NotificationButton extends Component {
   static propTypes = {
@@ -185,44 +187,12 @@ export class ToastNotification extends Component {
       { [`bx--toast-notification--${this.props.kind}`]: this.props.kind },
       className
     );
-    const a11yWarningIcon = (
-      <svg
-        class={`bx--${notificationType}-notification__icon`}
-        width="20"
-        height="20"
-        xmlns="http://www.w3.org/2000/svg">
-        <defs>
-          <path
-            d="M18.05 15.95L9.925.95a.625.625 0 0 0-1.1 0L.7 15.95a.625.625 0 0 0 0 .625.625.625 0 0 0 .55.3H17.5a.625.625 0 0 0 .55-.925z"
-            id="a"
-          />
-          <path
-            d="M.55.006h1.406v5H.55v-5zm.7 8.119a.938.938 0 1 1 0-1.875.938.938 0 0 1 0 1.875z"
-            id="b"
-          />
-        </defs>
-        <g fill="none" fill-rule="evenodd">
-          <path
-            d="M18.675 16.575l-8.125-15a.625.625 0 0 0-1.1 0l-8.125 15a.625.625 0 0 0 0 .625.625.625 0 0 0 .55.3h16.25a.625.625 0 0 0 .55-.925z"
-            fill="#FDD13A"
-          />
-          <g>
-            <path
-              d="M9.3 6.881h1.406v5H9.3v-5zM10 15a.938.938 0 1 1 0-1.875A.938.938 0 0 1 10 15z"
-              fill="#171717"
-              fill-rule="nonzero"
-            />
-          </g>
-          <path d="M0 0h20v20H0z" />
-        </g>
-      </svg>
-    );
     const NotificationIcon = kind => {
       switch (kind) {
         case 'info':
           return null;
         case 'warning':
-          return a11yWarningIcon;
+          return a11yIconWarningSolid(notificationType);
         default:
           return (
             <Icon
@@ -318,45 +288,12 @@ export class InlineNotification extends Component {
       className
     );
 
-    // temporary workaround for a11y warning icon. TODO: for @carbon/icons-react
-    const a11yWarningIcon = (
-      <svg
-        class={`bx--${notificationType}-notification__icon`}
-        width="20"
-        height="20"
-        xmlns="http://www.w3.org/2000/svg">
-        <defs>
-          <path
-            d="M18.05 15.95L9.925.95a.625.625 0 0 0-1.1 0L.7 15.95a.625.625 0 0 0 0 .625.625.625 0 0 0 .55.3H17.5a.625.625 0 0 0 .55-.925z"
-            id="a"
-          />
-          <path
-            d="M.55.006h1.406v5H.55v-5zm.7 8.119a.938.938 0 1 1 0-1.875.938.938 0 0 1 0 1.875z"
-            id="b"
-          />
-        </defs>
-        <g fill="none" fill-rule="evenodd">
-          <path
-            d="M18.675 16.575l-8.125-15a.625.625 0 0 0-1.1 0l-8.125 15a.625.625 0 0 0 0 .625.625.625 0 0 0 .55.3h16.25a.625.625 0 0 0 .55-.925z"
-            fill="#FDD13A"
-          />
-          <g>
-            <path
-              d="M9.3 6.881h1.406v5H9.3v-5zM10 15a.938.938 0 1 1 0-1.875A.938.938 0 0 1 10 15z"
-              fill="#171717"
-              fill-rule="nonzero"
-            />
-          </g>
-          <path d="M0 0h20v20H0z" />
-        </g>
-      </svg>
-    );
     const NotificationIcon = kind => {
       switch (kind) {
         case 'info':
           return null;
         case 'warning':
-          return a11yWarningIcon;
+          return a11yIconWarningSolid(notificationType);
         default:
           return (
             <Icon
