3cdcd2f15346d7b5c94c3812d3cc233202637503
Zack Stair <ZacharyStair@users.noreply.github.com>
2019-01-23 11:11:17 -0500

    feat(DropdownV2): allows consumers to specify render prop for dropdown items

--- src/components/DropdownV2/DropdownV2-story.js
+++ src/components/DropdownV2/DropdownV2-story.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf, action } from '@storybook/react';
 import DropdownV2 from '../DropdownV2';
+import DropdownItem from '../DropdownItem';
 import DropdownSkeleton from '../DropdownV2/Dropdown.Skeleton';
 import WithState from '../../tools/withState';
 
@@ -23,6 +24,23 @@ const items = [
   },
 ];
 
+const stringItems = ['Option 1', 'Option 2', 'Option 3'];
+
+const dropdownItems = [
+  { itemText: 'hello', value: 'hello', style: { opacity: 1 } },
+  { itemText: 'world', value: 'world', style: { opacity: 1 } },
+];
+
+const itemToElement = item => {
+  const itemAsArray = item.text.split(' ');
+  return (
+    <div>
+      <span>{itemAsArray[0]}</span>
+      <span style={{ color: 'red' }}> {itemAsArray[1]}</span>
+    </div>
+  );
+};
+
 storiesOf('DropdownV2', module)
   .addWithInfo(
     'default',
@@ -39,6 +57,63 @@ storiesOf('DropdownV2', module)
         />
       </div>
     )
+  )
+  .add(
+    'items as strings',
+    () => (
+      <div style={{ width: 300 }}>
+        <DropdownV2
+          label="Label"
+          items={stringItems}
+          onChange={action('onChange')}
+        />
+      </div>
+    ),
+    {
+      info: {
+        text: 'Rendering an array of strings as `items`',
+      },
+    }
+  )
+  .add(
+    'items as components',
+    () => (
+      <div style={{ width: 300 }}>
+        <DropdownV2
+          label="Label"
+          items={items}
+          itemToString={item => (item ? item.text : '')}
+          itemToElement={itemToElement}
+          onChange={action('onChange')}
+        />
+      </div>
+    ),
+    {
+      info: {
+        text: `Rendering items as custom components`,
+      },
+    }
+  )
+  .add(
+    'with DropdownItems',
+    () => (
+      <div style={{ width: 300 }}>
+        <DropdownV2
+          label="Label"
+          items={dropdownItems}
+          itemToString={item => (item ? item.itemText : '')}
+          itemToElement={DropdownItem}
+          onChange={action('onChange')}
+        />
+      </div>
+    ),
+    {
+      info: {
+        text: `
+          Using DropdownItem as the components to render. Has some kinks due to the onClick in the DropdownItem.
+        `,
+      },
+    }
   )
   .addWithInfo(
     'inline',
