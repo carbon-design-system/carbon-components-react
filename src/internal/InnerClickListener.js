d7649d859c75804830b5a8dd839e1b6fbc038612
Calvin-Cheung <calvin.cheung@ibm.com>
2019-01-31 12:42:24 -0500

    Fix failing tests

--- src/internal/InnerClickListener.js
+++ src/internal/InnerClickListener.js
@@ -40,7 +40,10 @@ export default class InnerClickListener extends React.Component {
   handleDocumentClick(event) {
     // Ensure that the target exists in the DOM before checking the element
     // documentElement necessary for IE
-    if (this.element.ownerDocument.documentElement.contains(event.target) && this.element) {
+    if (
+      this.element.ownerDocument.documentElement.contains(event.target) &&
+      this.element
+    ) {
       if (this.element.contains && !this.element.contains(event.target)) {
         this.props.onClickOutside(event);
       }
