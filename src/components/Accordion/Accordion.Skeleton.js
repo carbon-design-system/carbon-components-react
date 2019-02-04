5d4da500e890f07b439f09352f24ea5ea77123ac
tamio.honma <tamio.honma@apertomove.de>
2019-02-04 16:48:56 +0100

    refactor(AccordionSkeleton): Converted skeleton from class to functional component

--- src/components/Accordion/Accordion.Skeleton.js
+++ src/components/Accordion/Accordion.Skeleton.js
@@ -13,10 +13,22 @@ import { settings } from 'carbon-components';
 
 const { prefix } = settings;
 
-export default class AccordionSkeleton extends React.Component {
-  render() {
-    const item = (
-      <li className={`${prefix}--accordion__item`}>
+export default function AccordionSkeleton() {
+  const item = (
+    <li className={`${prefix}--accordion__item`}>
+      <button type="button" className={`${prefix}--accordion__heading`}>
+        <Icon
+          className={`${prefix}--accordion__arrow`}
+          icon={iconChevronRight}
+        />
+        <SkeletonText className={`${prefix}--accordion__title`} />
+      </button>
+    </li>
+  );
+  return (
+    <ul className={`${prefix}--accordion ${prefix}--skeleton`}>
+      <li
+        className={`${prefix}--accordion__item ${prefix}--accordion__item--active`}>
         <button type="button" className={`${prefix}--accordion__heading`}>
           <Icon
             className={`${prefix}--accordion__arrow`}
@@ -24,29 +36,15 @@ export default class AccordionSkeleton extends React.Component {
           />
           <SkeletonText className={`${prefix}--accordion__title`} />
         </button>
+        <div className={`${prefix}--accordion__content`}>
+          <SkeletonText width="90%" />
+          <SkeletonText width="80%" />
+          <SkeletonText width="95%" />
+        </div>
       </li>
-    );
-    return (
-      <ul className={`${prefix}--accordion ${prefix}--skeleton`}>
-        <li
-          className={`${prefix}--accordion__item ${prefix}--accordion__item--active`}>
-          <button type="button" className={`${prefix}--accordion__heading`}>
-            <Icon
-              className={`${prefix}--accordion__arrow`}
-              icon={iconChevronRight}
-            />
-            <SkeletonText className={`${prefix}--accordion__title`} />
-          </button>
-          <div className={`${prefix}--accordion__content`}>
-            <SkeletonText width="90%" />
-            <SkeletonText width="80%" />
-            <SkeletonText width="95%" />
-          </div>
-        </li>
-        {item}
-        {item}
-        {item}
-      </ul>
-    );
-  }
+      {item}
+      {item}
+      {item}
+    </ul>
+  );
 }
