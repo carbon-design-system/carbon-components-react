c6c8e9697b066880e44e5eb42218d21cb5109ea2
Alison Miller <alisonmi@us.ibm.com>
2019-02-08 10:51:19 -0500

    docs(examples): address review comments and use prebuilt css

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,5 +1,3 @@
-import 'carbon-components/scss/globals/scss/styles.scss';
-
 import React, { Component } from 'react';
 import { Accordion, AccordionItem } from 'carbon-components-react';
 import logo from './logo.svg';
