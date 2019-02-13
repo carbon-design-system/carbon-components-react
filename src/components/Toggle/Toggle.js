d81ab30fa9e8141e7d6aca70415a7edf66484b69
Akira Sudoh <asudoh@gmail.com>
2019-02-13 19:12:46 +0900

    fix(Toggle): hide on/off test for screen readers
    Refs IBM/carbon-components#1794.

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -56,9 +56,13 @@ const Toggle = ({
       />
 
       <label className={`${prefix}--toggle__label`} htmlFor={id}>
-        <span className={`${prefix}--toggle__text--left`}>{labelA}</span>
+        <span className={`${prefix}--toggle__text--left`} aria-hidden="true">
+          {labelA}
+        </span>
         <span className={`${prefix}--toggle__appearance`} />
-        <span className={`${prefix}--toggle__text--right`}>{labelB}</span>
+        <span className={`${prefix}--toggle__text--right`} aria-hidden="true">
+          {labelB}
+        </span>
       </label>
     </div>
   );
