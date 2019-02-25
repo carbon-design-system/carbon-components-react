25e789c7f9cae9d2199bfbd632619ed9238c2552
Akira Sudoh <asudoh@gmail.com>
2019-02-26 05:46:37 +0900

    chore(checkVersion): remove closure

--- src/tools/checkReactVersion.js
+++ src/tools/checkReactVersion.js
@@ -3,12 +3,10 @@ import warning from 'warning';
 import React from 'react';
 import { breakingChangesX } from '../internal/FeatureFlags';
 
-/* #__PURE__ */ (() => {
-  if (__DEV__ && breakingChangesX && semver.lt(React.version, '16.8.2')) {
-    warning(
-      false,
-      'The next major release of `carbon-components-react` requires ' +
-        '`16.8.2` or greater version of `react` library.'
-    );
-  }
-})();
+if (__DEV__ && breakingChangesX && semver.lt(React.version, '16.8.2')) {
+  warning(
+    false,
+    'The next major release of `carbon-components-react` requires ' +
+      '`16.8.2` or greater version of `react` library.'
+  );
+}
