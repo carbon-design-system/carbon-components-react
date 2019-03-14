f11c90f1ac39262418ec8ed8708920890034eb3f
Alessandra Davila <aledavila@ibm.com>
2019-03-14 10:48:10 -0500

    chore(search): update icon

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -11,6 +11,7 @@ import classNames from 'classnames';
 import { iconSearch, iconCloseSolid } from 'carbon-icons';
 import Search16 from '@carbon/icons-react/lib/search/16';
 import Close16 from '@carbon/icons-react/lib/close/16';
+import Close20 from '@carbon/icons-react/lib/close/20';
 import { settings } from 'carbon-components';
 import Icon from '../Icon';
 import { componentsX } from '../../internal/FeatureFlags';
@@ -139,6 +140,8 @@ export default class Search extends Component {
       [`${prefix}--search-close--hidden`]: !hasContent,
     });
 
+    const CloseIconX = !small ? Close20 : Close16;
+
     return (
       <div
         className={searchClasses}
@@ -177,7 +180,7 @@ export default class Search extends Component {
           type="button"
           aria-label={closeButtonLabelText}>
           {componentsX ? (
-            <Close16 aria-label={closeButtonLabelText} role="img" />
+            <CloseIconX />
           ) : (
             <Icon icon={iconCloseSolid} description={closeButtonLabelText} />
           )}
