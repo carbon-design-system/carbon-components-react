eeac69046477ca3c1b93e32379cc406133e7712a
Akira Sudoh <asudoh@gmail.com>
2019-03-19 07:11:10 +0900

    chore(test): mac support

--- scripts/ci-check.sh
+++ scripts/ci-check.sh
@@ -5,8 +5,11 @@ set -e
 yarn prettier:diff
 yarn lint
 yarn test --runInBand
-cp src/internal/FeatureFlags.js src/internal/FeatureFlags.js.orig
-sed -i -r 's/export *const *(components|breakingChanges)X *= *false/export const \1X = true/g' src/internal/FeatureFlags.js
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i.orig -E 's/export *const *(components|breakingChanges)X *= *false/export const \1X = true/g' src/internal/FeatureFlags.js
+else
+  sed -i.orig -r 's/export *const *(components|breakingChanges)X *= *false/export const \1X = true/g' src/internal/FeatureFlags.js
+fi
 yarn test --runInBand --updateSnapshot
 mv src/internal/FeatureFlags.js.orig src/internal/FeatureFlags.js
 yarn test-ssr
