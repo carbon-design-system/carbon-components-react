806ebde440a61aeb650ab2b344a3134cfacf5ab6
Alison Joseph <alisonejoseph@gmail.com>
2019-03-22 08:36:11 -0500

    fix: update skeleton svg for progress indicator

--- src/components/ProgressIndicator/ProgressIndicator.Skeleton.js
+++ src/components/ProgressIndicator/ProgressIndicator.Skeleton.js
@@ -7,19 +7,27 @@
 
 import React from 'react';
 import { settings } from 'carbon-components';
+import { componentsX } from '../../internal/FeatureFlags';
 
 const { prefix } = settings;
 
 export default class ProgressIndicatorSkeleton extends React.Component {
   render() {
+    const currentSvg = componentsX ? (
+      <svg>
+        <path d="M 7, 7 m -7, 0 a 7,7 0 1,0 14,0 a 7,7 0 1,0 -14,0" />
+      </svg>
+    ) : (
+      <svg>
+        <circle cx="12" cy="12" r="12" />
+        <circle cx="12" cy="12" r="6" />
+      </svg>
+    );
+
     const step = (
       <li
         className={`${prefix}--progress-step ${prefix}--progress-step--incomplete`}>
-        <svg>
-          <g>
-            <circle cx="12" cy="12" r="12" />
-          </g>
-        </svg>
+        {currentSvg}
         <p className={`${prefix}--progress-label`} />
         <span className={`${prefix}--progress-line`} />
       </li>
