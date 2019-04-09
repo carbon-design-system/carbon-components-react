3b0507679ffc5e062d06b1b89faf6aa1c70301d1
Jen Downs <hello@jendowns.com>
2019-04-09 15:28:00 -0500

    fix(data-table-skel): headers can be array of objs

--- src/components/DataTableSkeleton/DataTableSkeleton.js
+++ src/components/DataTableSkeleton/DataTableSkeleton.js
@@ -27,6 +27,16 @@ const DataTableSkeleton = ({
     [`${prefix}--data-table-v2--compact`]: compact,
   });
 
+  let normalizedHeaders;
+
+  if (typeof headers[0] === 'object') {
+    normalizedHeaders = headers.reduce((filtered, current) =>
+      filtered.push(current.header)
+    );
+  } else {
+    normalizedHeaders = headers;
+  }
+
   const rowRepeat = rowCount - 1;
   const rows = Array(rowRepeat);
   const columnsArray = Array.from({ length: columnCount }, (_, index) => index);
@@ -45,7 +55,7 @@ const DataTableSkeleton = ({
       <thead>
         <tr>
           {columnsArray.map(i => (
-            <th key={i}>{headers[i]}</th>
+            <th key={i}>{normalizedHeaders[i]}</th>
           ))}
         </tr>
       </thead>
@@ -88,7 +98,13 @@ DataTableSkeleton.propTypes = {
   /**
    * Optionally specify the displayed headers
    */
-  headers: PropTypes.array,
+  headers: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.shape({
+      key: PropTypes.string,
+      header: PropTypes.node,
+    }),
+  ]),
 };
 
 DataTableSkeleton.defaultProps = {
