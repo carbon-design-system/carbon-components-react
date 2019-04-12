2b8cc60106f443e9602e7252247114112c6310c6
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-04-11 20:57:13 -0500

    refactor: update SideNav state management

--- src/components/UIShell/SideNav.js
+++ src/components/UIShell/SideNav.js
@@ -5,131 +5,107 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import React, { useState, useRef } from 'react';
 import { settings } from 'carbon-components';
 import cx from 'classnames';
-import React from 'react';
 import PropTypes from 'prop-types';
 import { AriaLabelPropType } from '../../prop-types/AriaPropTypes';
 import SideNavFooter from './SideNavFooter';
 
 const { prefix } = settings;
 
-const translations = {
-  'carbon.sidenav.state.open': 'Close',
-  'carbon.sidenav.state.closed': 'Open',
+const SideNav = React.forwardRef(function SideNav(props, ref) {
+  const {
+    expanded: expandedProp,
+    defaultExpanded,
+    'aria-label': ariaLabel,
+    'aria-labelledby': ariaLabelledBy,
+    children,
+    className: customClassName,
+    translateById: t,
+  } = props;
+  const { current: controlled } = useRef(expandedProp != null);
+  const [expandedState, setExpandedState] = useState(defaultExpanded);
+  const expanded = controlled ? expandedProp : expandedState;
+
+  const handleExpand = ({ newState = !expanded }) => {
+    if (!controlled) {
+      setExpandedState(newState);
+    }
+  };
+
+  const accessibilityLabel = {
+    'aria-label': ariaLabel,
+    'aria-labelledby': ariaLabelledBy,
+  };
+
+  const assistiveText = expanded
+    ? t('carbon.sidenav.state.open')
+    : t('carbon.sidenav.state.closed');
+
+  const className = cx({
+    [`${prefix}--side-nav`]: true,
+    [`${prefix}--side-nav--expanded`]: expanded,
+    [customClassName]: !!customClassName,
+  });
+
+  return (
+    <nav
+      ref={ref}
+      className={`${prefix}--side-nav__navigation ${className}`}
+      {...accessibilityLabel}
+      onFocus={() => handleExpand({ newState: true })}
+      onBlur={() => handleExpand({ newState: false })}>
+      {children}
+      <SideNavFooter
+        assistiveText={assistiveText}
+        isExpanded={expanded}
+        onToggle={handleExpand}
+      />
+    </nav>
+  );
+});
+
+SideNav.defaultProps = {
+  translateById: id => {
+    const translations = {
+      'carbon.sidenav.state.open': 'Close',
+      'carbon.sidenav.state.closed': 'Open',
+    };
+    return translations[id];
+  },
+  isExpanded: false,
 };
 
-function translateById(id) {
-  return translations[id];
-}
+SideNav.propTypes = {
+  /**
+   * If `true`, the SideNav will be expanded, otherwise it will be collapsed.
+   * Using this prop causes SideNav to become a controled component.
+   */
+  expanded: PropTypes.bool,
 
-export default class SideNav extends React.Component {
-  static propTypes = {
-    /**
-     * Specify whether the side navigation is expanded or collapsed
-     */
-    isExpanded: PropTypes.bool,
-    /**
-     * Required props for accessibility label on the underlying menu
-     */
-    ...AriaLabelPropType,
+  /**
+   * If `true`, the SideNav will be open on initial render.
+   */
+  defaultExpanded: PropTypes.bool,
 
-    /**
-     * Optionally provide a custom class to apply to the underlying <li> node
-     */
-    className: PropTypes.string,
+  /**
+   * Required props for accessibility label on the underlying menu
+   */
+  ...AriaLabelPropType,
 
-    /**
-     * Provide a custom function for translating all message ids within this
-     * component. This function will take in two arguments: the mesasge Id and the
-     * state of the component. From this, you should return a string representing
-     * the label you want displayed or read by screen readers.
-     */
-    translateById: PropTypes.func,
-  };
+  /**
+   * Optionally provide a custom class to apply to the underlying <li> node
+   */
+  className: PropTypes.string,
 
-  static defaultProps = {
-    translateById,
-    isExpanded: false,
-  };
+  /**
+   * Provide a custom function for translating all message ids within this
+   * component. This function will take in two arguments: the mesasge Id and the
+   * state of the component. From this, you should return a string representing
+   * the label you want displayed or read by screen readers.
+   */
+  translateById: PropTypes.func,
+};
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      prevIsExpanded: props.isExpanded,
-      isExpanded: props.isExpanded,
-      isFocused: false,
-    };
-  }
-
-  static getDerivedStateFromProps({ isExpanded }, state) {
-    return state && state.prevIsExpanded === isExpanded
-      ? null
-      : {
-          prevIsExpanded: isExpanded,
-          isExpanded,
-        };
-  }
-
-  handleExpand = () => {
-    this.setState(state => ({ isExpanded: !state.isExpanded }));
-  };
-
-  handleFocus = () => {
-    this.setState(state => {
-      if (!state.isFocused) {
-        return { isFocused: true };
-      }
-      return null;
-    });
-  };
-
-  handleBlur = () => {
-    this.setState(state => {
-      if (state.isFocused) {
-        return { isFocused: false };
-      }
-      return null;
-    });
-  };
-
-  render() {
-    const {
-      'aria-label': ariaLabel,
-      'aria-labelledby': ariaLabelledBy,
-      children,
-      className: customClassName,
-      translateById: t,
-    } = this.props;
-    const { isExpanded, isFocused } = this.state;
-    const accessibilityLabel = {
-      'aria-label': ariaLabel,
-      'aria-labelledby': ariaLabelledBy,
-    };
-    const assistiveText =
-      isExpanded || isFocused
-        ? t('carbon.sidenav.state.open')
-        : t('carbon.sidenav.state.closed');
-    const className = cx({
-      [`${prefix}--side-nav`]: true,
-      [`${prefix}--side-nav--expanded`]: isExpanded || isFocused,
-      [customClassName]: !!customClassName,
-    });
-
-    return (
-      <nav
-        className={`${prefix}--side-nav__navigation ${className}`}
-        {...accessibilityLabel}
-        onFocus={this.handleFocus}
-        onBlur={this.handleBlur}>
-        {children}
-        <SideNavFooter
-          assistiveText={assistiveText}
-          isExpanded={isExpanded}
-          onToggle={this.handleExpand}
-        />
-      </nav>
-    );
-  }
-}
+export default SideNav;
