6d085aa24868a5f12599149ed188e596e5056df2
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-04-22 13:55:14 -0500

    fix: add a11y updates

--- src/components/DataTable/TableToolbar.js
+++ src/components/DataTable/TableToolbar.js
@@ -4,16 +4,14 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+import React from 'react';
 import { settings } from 'carbon-components';
-import wrapComponent from '../../tools/wrapComponent';
 
 const { prefix } = settings;
 
-const TableToolbar = wrapComponent({
-  name: 'TableToolbar',
-  type: 'section',
-  className: `${prefix}--table-toolbar`,
-});
-
+const TableToolbar = ({ children }) => (
+  <section role="group" className={`${prefix}--table-toolbar`}>
+    {children}
+  </section>
+);
 export default TableToolbar;
