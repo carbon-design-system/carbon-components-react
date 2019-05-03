6a3a433eebaa78616f19e878fd58757fedd30b78
emyarod <afw5059@gmail.com>
2019-05-03 07:35:48 -0500

    refactor(MultiSelect): remove v9 code

--- src/components/MultiSelect/MultiSelect-story.js
+++ src/components/MultiSelect/MultiSelect-story.js
@@ -18,7 +18,6 @@ import {
 import { withReadme } from 'storybook-readme';
 import readme from './README.md';
 import MultiSelect from '../MultiSelect';
-import { componentsX } from '../../internal/FeatureFlags';
 
 const items = [
   {
@@ -54,12 +53,8 @@ const types = {
 
 const props = () => ({
   id: text('MultiSelect ID (id)', 'carbon-multiselect-example'),
-  ...(componentsX
-    ? {
-        titleText: text('Title (titleText)', 'Multiselect title'),
-        helperText: text('Helper text (helperText)', 'This is not helper text'),
-      }
-    : {}),
+  titleText: text('Title (titleText)', 'Multiselect title'),
+  helperText: text('Helper text (helperText)', 'This is not helper text'),
   filterable: boolean(
     'Filterable (`<MultiSelect.Filterable>` instead of `<MultiSelect>`)',
     false
