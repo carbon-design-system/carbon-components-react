9eff9fa83eea458c9e2e9c2787dfe80832d971e1
emyarod <afw5059@gmail.com>
2019-05-06 10:59:01 -0500

    docs: revert comment changes

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -101,67 +101,80 @@ TextInput.PasswordInput = PasswordInput;
 TextInput.ControlledPasswordInput = ControlledPasswordInput;
 TextInput.propTypes = {
   /**
-   * Provide a custom className that is applied directly to the underlying
-   * <input> node
+   * Specify an optional className to be applied to the <input> node
    */
   className: PropTypes.string,
+
   /**
    * Optionally provide the default value of the <input>
    */
   defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
   /**
-   * Specify whether the control is disabled
+   * Specify whether the <input> should be disabled
    */
   disabled: PropTypes.bool,
+
   /**
-   * Provide a unique identifier for the input field
+   * Specify a custom `id` for the <input>
    */
   id: PropTypes.string.isRequired,
+
   /**
    * Provide the text that will be read by a screen reader when visiting this
    * control
    */
   labelText: PropTypes.node.isRequired,
+
   /**
    * Optionally provide an `onChange` handler that is called whenever <input>
    * is updated
    */
   onChange: PropTypes.func,
+
   /**
    * Optionally provide an `onClick` handler that is called whenever the
    * <input> is clicked
    */
   onClick: PropTypes.func,
+
   /**
    * Specify the placeholder attribute for the <input>
    */
   placeholder: PropTypes.string,
+
   /**
-   * Specify the type attribute for the <input>
+   * Specify the type of the <input>
    */
   type: PropTypes.string,
+
   /**
-   * Provide the current value of the <input>
+   * Specify the value of the <input>
    */
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
   /**
-   * Specify whether or not the underlying label is visually hidden
+   * Specify whether you want the underlying label to be visually hidden
    */
   hideLabel: PropTypes.bool,
+
   /**
    * Specify whether the control is currently invalid
    */
   invalid: PropTypes.bool,
+
   /**
    * Provide the text that is displayed when the control is in an invalid state
    */
   invalidText: PropTypes.string,
+
   /**
    * Provide text that is used alongside the control label for additional help
    */
   helperText: PropTypes.node,
+
   /**
-   * Specify light version or default version of this control
+   * `true` to use the light version.
    */
   light: PropTypes.bool,
 };
