97826af1cd5ace618baf6a5440ae5f2c95877f6c
Jase Snider <jeff.snider@jaseone.com>
2019-09-10 16:56:08 -0500

    feat(MultiSelect): pass through  translations (cherry-pick from v7) (#2413)

--- src/components/MultiSelect/MultiSelect.js
+++ src/components/MultiSelect/MultiSelect.js
@@ -240,6 +240,7 @@ export default class MultiSelect extends React.Component {
                     <ListBox.Selection
                       clearSelection={!disabled ? clearSelection : noop}
                       selectionCount={selectedItem.length}
+                      translateWithId={translateWithId}
                     />
                   )}
                   <span className={`${prefix}--list-box__label`}>{label}</span>
